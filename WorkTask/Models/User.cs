e29520bfd93d672ce71da86bc0d3c9e7df1b3a0a
Микола Столярчук <mukola1511@gmail.com>
2024-09-05 14:04:51 +0300

    Added User model

---
+++ WorkTask/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace WorkTask.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
