887ccfbdeef65cdba2d65fc699ed2bbdc86a48cc
Mykola <112500011+Mukola11@users.noreply.github.com>
2024-09-08 02:24:05 +0300

    Define IUserService interface

---
+++ WorkTask/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using WorkTask.Models;
+
+namespace WorkTask.Services
+{
+    public interface IUserService
+    {
+        Task<bool> UserExists(string email, string username);
+        Task CreateUserAsync(User user);
+        Task<User> GetUserByEmailOrUsernameAsync(string usernameOrEmail);
+    }
+
+}
