77219bb304db7cce9989080f3e94424b79a29dd1
Mykola <112500011+Mukola11@users.noreply.github.com>
2024-09-10 02:51:23 +0300

    Add ITaskService interface for task management with filtering, sorting, and pagination support

---
+++ WorkTask/Services/ITaskService.cs
@@ -0,0 +1,15 @@
+﻿using WorkTask.Models;
+using static WorkTask.Models.SortingOptions;
+
+namespace WorkTask.Services
+{
+    public interface ITaskService
+    {
+        Task<UserTask> CreateTaskAsync(UserTaskDto taskDto, Guid userId);
+        Task<IEnumerable<UserTaskDto>> GetTasksAsync(Guid userId, TaskFilterDto filter, SortByOptions sortBy, SortOrder sortOrder);
+        Task<UserTaskDto> GetTaskByIdAsync(Guid id, Guid userId);
+        Task<UserTaskDto> UpdateTaskAsync(Guid id, Guid userId, UserTaskDto taskDto);
+        Task<bool> DeleteTaskAsync(Guid id, Guid userId);
+
+    }
+}
