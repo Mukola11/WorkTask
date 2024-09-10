0e117f76a6197fce07343bc3e9abd05969a320c9
Mykola <112500011+Mukola11@users.noreply.github.com>
2024-09-10 03:35:59 +0300

    Add TaskFilterDto for task filtering and sorting

---
+++ WorkTask/Models/TaskFilterDto.cs
@@ -0,0 +1,13 @@
+﻿namespace WorkTask.Models
+{
+    public class TaskFilterDto
+    {
+        public TaskStatus? Status { get; set; }
+        public DateTime? DueDate { get; set; }
+        public TaskPriority? Priority { get; set; }
+
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+
+}
